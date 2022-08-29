ccefbd8cc2ecb85c5128c2493f5667f5fe6cc703
RominaVargas <rominita.malfoy@gmail.com>
2022-08-29 13:04:57 -0400

    inicio proyecto

---
+++ src/lib/router/router.js
@@ -0,0 +1,12 @@
+import { welcomeView } from "../views/welcome.js"
+
+export const changeRoute = (hash) => {
+    const root = document.getElementById('root');
+    switch (hash) {
+        case '#/':
+            root.appendChild(welcomeView());
+            //se debe importar el logo y el boton para iniciar sesion desde welcome.js
+            break;
+    }
+
+};
---
+++ src/lib/views/welcome.js
@@ -0,0 +1,30 @@
+// export default () => /*html*/`
+//     <img src=/src/images/tutip logo.png>
+//     <h2>hay que compartip</h2>
+// `;
+
+// console.log('aparezco');
+
+export const welcomeView = () => {
+    const divWelcome = document.createElement('div');
+    divWelcome.className = 'welcomeContainer';
+    divWelcome.setAttribute('id', 'welcomeContainer');
+    // const divLogo = document.createElement('div');
+    // divLogo.className = 'logoContainer';
+    // const divLoginButton = document.createElement('div');
+    // divLoginButton.className = 'loginButtonContainer';
+    const welcomePage = /*html*/ ` 
+    <div class= "logoContainer">
+    <img src="./images/tutipLogo.png">
+    <h2> hay que compartip </h2>
+    </div>
+    <div class= "loginButtonContainer">
+    <button class= "loginButton">Iniciar Sesión</button>
+    <p>¿No tienes cuenta? <a href="#/signUp">Registrate aqui</a></p>
+    </div> 
+    `;
+    //document.innerHTML = welcomePage;
+    //document.getElementsByClassName('welcomeContainer').innerHTML += welcomePage;
+    divWelcome.appendChild(welcomePage);
+    return divWelcome;
+};
