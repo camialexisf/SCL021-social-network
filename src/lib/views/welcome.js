a2ff50d5622315109fdac0bb3cda9f7e24bc1381
francisca-rivera <franda.chi@gmail.com>
2022-09-03 12:46:56 -0400

    tres vistas completadas y pagina error hecha

--- src/lib/views/welcome.js
+++ src/lib/views/welcome.js
@@ -1,31 +1,36 @@
 const welcomeView = () => {
-  const welcomeViewContainer = document.createElement("div");
-  const welcomeLogoContainer = document.createElement("div");
-    welcomeLogoContainer.className = "welcomeLogoContainer";
-  const welcomeLogoImagen = document.createElement("img");
-    welcomeLogoImagen.className = "welcomeLogoImagen";
-    welcomeLogoImagen.src = "./images/tutipWithSubtitle.png";
+  const welcomeViewContainer = document.createElement('div');
+  const welcomeLogoContainer = document.createElement('div');
+  welcomeLogoContainer.className = 'welcomeLogoContainer';
+  const welcomeLogoImagen = document.createElement('img');
+  welcomeLogoImagen.className = 'welcomeLogoImagen';
+  welcomeLogoImagen.src = './images/tutipWithSubtitle.png';
   welcomeLogoContainer.appendChild(welcomeLogoImagen);
-  const welcomeLoginButtonContainer = document.createElement("div");
-  const welcomeLoginButton = document.createElement("button");
-    welcomeLoginButton.className = "welcomeLoginButton";
-  const logInLink = document.createElement("a");
-    logInLink.href = "#/logIn";
-    welcomeLoginButton.innerText = "Iniciar sesión";
-   logInLink.appendChild(welcomeLoginButton);
-  const signUpText = document.createElement("p");
-    signUpText.innerText = "¿No tienes cuenta?";
-  const signUpLink = document.createElement("a");
-    signUpLink.innerText = "Registrate aquí";
-    signUpLink.href = "#/signUp";
-   signUpText.appendChild(signUpLink);
-   welcomeLoginButtonContainer.appendChild(logInLink);
-   welcomeLoginButtonContainer.appendChild(signUpText);
-   welcomeViewContainer.appendChild(welcomeLogoContainer);
-   welcomeViewContainer.appendChild(welcomeLoginButtonContainer);
-  const footer = document.createElement("p");
-    footer.innerText = "©Copyright 2022";
-   welcomeViewContainer.appendChild(footer);
+
+  const welcomeLoginButtonContainer = document.createElement('div');
+  const welcomeLoginButton = document.createElement('button');
+  welcomeLoginButton.className = 'welcomeLoginButton';
+  const logInLink = document.createElement('a');
+  logInLink.href = '#/logIn';
+  logInLink.className = 'logInLink';
+  welcomeLoginButton.innerText = 'Iniciar sesión';
+  logInLink.appendChild(welcomeLoginButton);
+
+  const signUpText = document.createElement('p');
+  signUpText.innerText = '¿No tienes cuenta? ';
+  const signUpLink = document.createElement('a');
+  signUpLink.innerText = ' Registrate aquí';
+  signUpLink.href = '#/signUp';
+  signUpText.appendChild(signUpLink);
+  welcomeLoginButtonContainer.appendChild(logInLink);
+  welcomeLoginButtonContainer.appendChild(signUpText);
+  welcomeViewContainer.appendChild(welcomeLogoContainer);
+  welcomeViewContainer.appendChild(welcomeLoginButtonContainer);
+
+  const footer = document.createElement('p');
+  footer.innerText = '©Copyright 2022';
+  footer.className = 'footer';
+  welcomeViewContainer.appendChild(footer);
 
   return welcomeViewContainer;
 };
