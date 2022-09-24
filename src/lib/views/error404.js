894c7ba708137dd7d0063a82231c85334e3f3e9f
RominaVargas <rominita.malfoy@gmail.com>
2022-09-24 13:44:37 -0300

    cambios css en todo el proyecto

--- src/lib/views/error404.js
+++ src/lib/views/error404.js
@@ -1,7 +1,15 @@
+import navBar from './navBar.js';
+
 const error404 = () => {
+  const errorViewContainer = document.createElement('div');
   const errorText = document.createElement('p');
   errorText.innerText = 'Ups aqui no hay nada aún';
 
-  return errorText;
+  errorViewContainer.appendChild(errorText);
+  errorViewContainer.appendChild(navBar());
+  return errorViewContainer;
 };
+
+
+
 export default error404;
